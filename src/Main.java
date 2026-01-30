cd758bd73ed1b3a714f76199f6d296cd0fe219ee
SwaritSharma <swarit.developer@gmail.com>
2026-01-30 22:07:05 +0530

    Submission of Linear and Binary Search

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import Sorting.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 1, 4, 2, 8};
+
+        System.out.println("Before sorting:");
+        System.out.println(Arrays.toString(arr));
+//        System.out.println();
+        Merge.sort(0,arr.length-1,arr);
+//        System.out.println();
+        System.out.println("After sorting:");
+        System.out.println(Arrays.toString(arr));
     }
 }
